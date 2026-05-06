73c2abe8b4eb8fdc23cd2d93449da0e3c26459ae
Asif Khan <asifjamshed4@gmail.com>
2026-05-06 14:20:12 +0530

    Day-1:- roadmap

---
+++ day-1/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur,
+        perferendis?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, quas?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, eum.
+      </p>
+    </>
+  );
+}
+
+export default App;
