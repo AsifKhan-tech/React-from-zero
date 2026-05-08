a89ecff8b1099d633d4a4e9bb2ea1ea395759d01
Asif Khan <asifjamshed4@gmail.com>
2026-05-08 18:02:48 +0530

    understand react flow & structure

---
+++ day-3/src/App.jsx
@@ -0,0 +1,12 @@
+import Test from "./components/Test.jsx";
+
+function App() {
+  return (
+    <>
+      <h1>Day-3: Understand React flow & Structure</h1>
+      <Test />
+    </>
+  );
+}
+
+export default App;
---
+++ day-3/src/components/Test.jsx
@@ -0,0 +1,10 @@
+const Test = () => {
+  return (
+    <>
+      <h3>This is the component</h3>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </>
+  );
+};
+
+export default Test;
